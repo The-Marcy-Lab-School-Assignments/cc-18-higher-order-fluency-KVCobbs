de0f93b7e5cbe40a889abdf0c422563aae756e7d
KVCobbs <kenyacobbs9001@gmail.com>
2021-12-08 15:13:20 +0000

    done on my own

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Code Challenge: Control Flow
+tio# Code Challenge: Control Flow
 
 ## Instructions
 
--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 //write your code here
+function shortWords(words) {
+    //filter 
+    return words.filter(word => word.length < 5)
+    /*
+    return arr.filter(function(arr){
+        return arr.length < 5;
+    })
+    */
+}
+
+function noVowel(words) {
+    // filter and replace
+  //  return words.filter(word => word./[a-z][aeiouy][a-z]/ )
+}
+
+function longToShort(words){
+    // sort 
+    return words.sort((a,b) => {return b.length - a.length})
+    
+  //  return arr1.length - arr2.length
+}
+
+function onlyVowelA (words) {
+    //filter 
+}
+
+function pluralize (words) {
+  // map
+  return words.map((words) => {return `$words}s`})
+}
+
+function longerThanSeven (words) {
+    
+}
+
+function oddLength (words) {
+    //filter 
+    
+    return words.filter(word => word.length % 2 === 1)
+}
+
+function allFour (words) {
+    
+}
+
+function sum(numbers){
+    //reduce
+    return numbers.reduce((prev, curr) => return prev + curr)
+}
+
+function longWords(words) {
+    return words.filter(word => word.length < 3)
+}
+
+function uppercase(words) {
+    return words.map((word => {return word[0].toUpperCase() + word.substring[1]})
+}
+
+function concatStrings(words) {
+    // reduce 
+    return words.reduce('string') => 
+}
